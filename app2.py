a1d158f7c4bf7b9b0ac92a328a820b68d438cc6a
amaldonado8 <amaldonado@sicobra.com>
2026-04-09 09:41:49 -0500

    Refactor app2.py to improve data processing

--- app2.py
+++ app2.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="Reporte Tiempo Sin Gestión r", layout="wide")
+st.set_page_config(page_title="Reporte Tiempo Sin Gestión", layout="wide")
 
-st.title(" Reporte Tiempo Sin Gestión")
+st.title(" Reporte Tiempo Sin Gestión _")
 
 
 # 1. CARGAR ARCHIVOS
